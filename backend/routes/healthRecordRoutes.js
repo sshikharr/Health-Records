b5bf26bdee34d7c9dd72e31a040c0904dcfaf593
sshikharr <shikhardwivedi2019@gmail.com>
2024-09-19 10:29:32 +0530

    Added getting single record route

--- backend/routes/healthRecordRoutes.js
+++ backend/routes/healthRecordRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getHealthRecords, createHealthRecord, updateHealthRecord, deleteHealthRecord } = require('../controllers/healthRecordController');
+const { getHealthRecords, createHealthRecord, updateHealthRecord, deleteHealthRecord, getHealthRecordById } = require('../controllers/healthRecordController');
 const auth = require('../middleware/authMiddleware')
 const router = express.Router();
 
@@ -14,5 +14,8 @@ router.put('/:id', auth, updateHealthRecord);
 
 // Delete a record
 router.delete('/:id', auth, deleteHealthRecord);
+
+// Single record
+router.get('/:id', auth, getHealthRecordById)
 
 module.exports = router;
